ca83e67218c3559d63a1cd462b83815bfa4442c1
bengold <benjamingold@ufl.eud>
2023-10-25 18:30:59 -0400

    lab 6

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+'''
+Lab 6: Version Control
+Benjamin Gold
+'''
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def encoder(num_string):
+    int_string = ""
+    for i in range(0, len(num_string)):
+        new_num = int(num_string[i]) + 3
+        if new_num > 9:
+            new_num = str(int(num_string[i] - 10))
+        int_string += str(new_num)
+    return int_string
 
+def decoder():
+    pass
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    run = True
+    encoded_pass = ""
+    decoded_pass = ""
+    while run:
+        print("""Menu
+-------------
+1. Encode
+2. Decode
+3. Quit""")
+        user_input = input("Please enter an option: ")
+        if user_input == "1":
+            entered_str = input("Please enter your password to encode: ")
+            encoded_pass = encoder(entered_str)
+            print("Your password has been encoded and stored!")
+        if user_input == "2":
+            print(f"The encoded password is {encoded_pass}, and the original password is {entered_str}")
+        if user_input == "3":
+            quit()
